1279f142ecbb516138ed93c667f31cd553d42cc7
Mateusz Kiciński | Atrem <mateusz.kicinski@atrem.pl>
2018-05-01 19:42:33 +0200

    Create task component test - issues

--- src/app/components/create-task/create-task.component.spec.ts
+++ src/app/components/create-task/create-task.component.spec.ts
@@ -1,19 +1,28 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {CreateTaskComponent} from './create-task.component';
+import {MaterialModule} from '../../material.module';
+import {FormsModule} from '@angular/forms';
+import {RoomService} from '../../services/room.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('CreateTaskComponent', () => {
   let component: CreateTaskComponent;
   let fixture: ComponentFixture<CreateTaskComponent>;
+  let mockService: RoomService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CreateTaskComponent]
+      declarations: [CreateTaskComponent],
+      imports: [MaterialModule, FormsModule, HttpClientModule],
+      providers: [RoomService]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
+    mockService = {getPriorities: null} as RoomService;
+    spyOn(mockService, 'getPriorities').and.returnValue([1, 2]);
     fixture = TestBed.createComponent(CreateTaskComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
