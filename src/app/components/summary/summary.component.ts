1a40abd5c82b9d2ff0632122d4ba5f8de18e7448
MaciejWWojcik <maciejwwojcik@gmail.com>
2018-05-21 16:15:03 +0200

    Summary view

---
+++ src/app/components/summary/summary.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from "@angular/material";
+import {RoomService} from "../../services/room.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.css']
+})
+export class SummaryComponent implements OnInit {
+
+  roomId:string;
+  summary: Summary;
+
+  constructor(private route: ActivatedRoute, private service:RoomService) { }
+
+  ngOnInit() {
+    this.roomId = this.route.snapshot.params.id;
+    this.service.getSummary(this.roomId).subscribe(
+      data => this.summary = data,
+      error => console.error(error)
+    )
+  }
+
+}
+
+export interface Summary{
+  date:string;
+  roomName: string;
+  tasks: {name:string, estimate:string}[];
+  participants: {name:string}[];
+}
