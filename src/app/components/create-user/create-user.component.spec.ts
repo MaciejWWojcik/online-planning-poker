d20ab02772af2a76e21bff7f5fec57c3b5d64726
MaciejWWojcik <maciejwwojcik@gmail.com>
2018-06-04 21:17:44 +0200

    test update

--- src/app/components/create-user/create-user.component.spec.ts
+++ src/app/components/create-user/create-user.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateUserComponent } from './create-user.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Router, RouterModule} from "@angular/router";
+import {RoomService} from "../../services/room.service";
+import {FormsModule} from "@angular/forms";
+import {MaterialModule} from "../../material.module";
+import {HttpClientModule} from "@angular/common/http";
+import {MatDialog, MatDialogModule, MatDialogRef, MatToolbar} from "@angular/material";
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from "@angular/core";
+import {BrowserDynamicTestingModule} from "@angular/platform-browser-dynamic/testing";
 
 describe('CreateUserComponent', () => {
   let component: CreateUserComponent;
@@ -8,12 +18,29 @@ describe('CreateUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateUserComponent ]
+      declarations: [ CreateUserComponent ],
+      imports: [MatDialogModule,MaterialModule, FormsModule, HttpClientModule, RouterModule, BrowserModule,BrowserAnimationsModule],
+      providers: [MatDialog, MatToolbar, RoomService, HttpClientModule,
+        {provide: MatDialogRef, useValue: {}},
+        {
+          provide: Router, useClass: class {
+            navigate = jasmine.createSpy("navigate");
+          }
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [CreateUserComponent]
+      }
+    });
+
     fixture = TestBed.createComponent(CreateUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
