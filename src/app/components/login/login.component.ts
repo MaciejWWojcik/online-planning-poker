754c09bef0711ab0a7a38a2c733255544688931f
MaciejWWojcik <maciejwwojcik@gmail.com>
2018-06-09 16:12:30 +0200

    integrated

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {MatDialogRef, MatSnackBar} from "@angular/material";
+import {Account, AccountService} from "../../services/account.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
+
+  constructor(public dialogRef: MatDialogRef<LoginComponent>, private account: AccountService, private info: MatSnackBar) {
+  }
+
+  ngOnInit() {
+  }
+
+  signIn() {
+    this.account.signIn(this.email, this.password).subscribe(
+      data => {
+        console.log(data);
+        this.account.account = data;
+        this.dialogRef.close(this.email);
+      },
+      error => this.info.open('Error, please try again later', '', {duration:3000})
+    );
+  }
+}
